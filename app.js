30f89a38f50d628c73aa11c9f2fcea96ab9eae55
Bilal Awshid <165455288+BilalR4M@users.noreply.github.com>
2025-03-01 18:25:02 +0530

    write file function

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ fs.readFile('file.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
+
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+}); 
