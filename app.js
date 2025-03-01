7d22782f084ecc369e5d4cbf6f245efd50beb7c7
Bilal Awshid <165455288+BilalR4M@users.noreply.github.com>
2025-03-01 18:28:09 +0530

    Created web server

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 console.log("Hello World");
 
+//read file 
 const fs = require('fs');
 fs.readFile('file.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
 
+//write file
 const fs = require('fs');
 fs.writeFile('file.txt', 'Hello World', (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
 }); 
+
+//web server 
+
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
